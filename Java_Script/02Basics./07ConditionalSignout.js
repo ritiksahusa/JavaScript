f5cb1b194d7511c76dc9cd74e59bf78e6d36e1eb
ritiksahusa <47048937+ritiksahusa@users.noreply.github.com>
2020-08-27 08:18:08 +0530

    Add files via upload

---
+++ Java_Script/02Basics./07ConditionalSignout.js
@@ -0,0 +1,15 @@
+// show user a signout button if he is authenticated,
+//other-wise show him option to login /signin
+
+var authenticated = falses;
+
+// if(authenticated){
+//     console.log("Show signout button");
+// }
+// else{
+//     console.log("Show login option");
+// }
+
+
+/* ternary opreater*/
+authenticated ? console.log("signout button") : console.log("Login");
