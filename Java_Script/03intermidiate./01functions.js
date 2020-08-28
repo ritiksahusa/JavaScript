b80390646866a2fdc6773bbbbc38b5666b272c20
ritiksahusa <47048937+ritiksahusa@users.noreply.github.com>
2020-08-28 08:13:20 +0530

    Add files via upload

---
+++ Java_Script/03intermidiate./01functions.js
@@ -0,0 +1,27 @@
+//functional programming
+/* objectives:
+1. how to declare a function
+2. how to pass a variable
+3.  print using different format
+4. how to use return in function 
+5 how to access return value
+*/
+function sayHello(name){
+    console.log("hello there, Ritik"); 
+    console.log("hello there ",name);  
+    console.log(`hello there , ${name}. how are you`);
+    return `hello , ${name}ist js`;
+}
+name= "ritik"
+// console.log(sayHello(name));
+// sayHello(name); //calling a fuction
+// sayHello("sahu")
+// sayHello;// references a function
+
+function namstey(){
+    return "hello  india";
+}
+var greetings= namstey();
+// console.log(namstey());
+// console.log(greetings);
+
